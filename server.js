8cae9d3a6a636d13b91b700360dcaad5af691bdb
asforesXD <matthew9252@naver.com>
2020-12-04 12:56:59 +0900

    uhh..

--- server.js
+++ server.js
@@ -4,11 +4,24 @@ const { text } = require('body-parser');
 const express = require("express");
 const { response } = require("express");
 const server = express();
+const hbs = require('express-handlebars');
+
+server.engine(
+    'hbs',
+    hbs({
+        extname: 'hbs',
+        defaultLayout: 'layout',
+        layoutsDir: __dirname+'/views/layouts',
+        partialsDir: __dirname+'views/partials'
+    })
+);
+
+server.set("View engine", 'hbs');
 
 server.use(express.static(__dirname + '/public'));
 
 server.get('/', (req,res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.status(200).render('index.hbs', {Mt});
 
 })
 server.listen(3000,() => {
