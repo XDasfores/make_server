ba42960d2bfffa716202ae8fdba89583982c2c2b
asforesXD <matthew9252@naver.com>
2020-12-04 13:22:39 +0900

    d

--- server.js
+++ server.js
@@ -6,26 +6,9 @@ const { response } = require("express");
 const server = express();
 const hbs = require('express-handlebars');
 
-server.engine(
-    'hbs',
-    hbs({
-        extname: 'hbs',
-        defaultLayout: 'layout',
-        layoutsDir: __dirname+'/views/layouts',
-        partialsDir: __dirname+'views/partials'
-    })
-);
 
-server.set("View engine", 'hbs');
-
-server.use(express.static(__dirname + '/public'));
-
-server.get('/', (req,res) => {
-    res.status(200).render('index.hbs', {Mt});
-
-})
 server.listen(3000,() => {
-    console.log("The server is runing on port 3000 prodect by MT")
+    console.log("The server is runing on port 3000 apply by MT")
 
 });
 
